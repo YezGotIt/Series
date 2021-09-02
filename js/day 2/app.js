b35c41f3afba83041d7188525337d21fa79ac1da
YezGotIt <83292822+YezGotIt@users.noreply.github.com>
2021-09-02 11:59:38 +0530

    Add files via upload

---
+++ js/day 2/app.js
@@ -0,0 +1,22 @@
+var number = 2
+console.log('number:', number)
+var string = "string"
+console.log('string:', string)
+var isBool = true
+console.log('isBool:', isBool)
+var isNull = null
+console.log('isNull:', isNull)
+var isUndefined = undefined
+console.log('isUndefined:', isUndefined)
+
+var array = [1, 2, 3, 4]
+console.log('array:', array)
+
+var object = {name:"bb"}
+console.log('object:', object)
+
+function name(){
+    console.log('name function:', name)
+}
+
+console.log("function calling", name())
